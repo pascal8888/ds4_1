332e7573860ce5e77389ceb023565e4d323cd1f7
James Schacht <pascal888@yahoo.com>
2015-06-03 09:30:06 -0400

    Signed-off-by: James Schacht <pascal888@yahoo.com>

--- plot1.R
+++ plot1.R
@@ -1,1 +1,5 @@
 ## Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+y1999 <- which(NEI$year == 1999)
+y2002 <- which(NEI$year == 2002)
+y2005 <- which(NEI$year == 2005)
+y2005 <- which(NEI$year == 2008)
